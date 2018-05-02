cc079b1022dc4ab2e2e51b6e76af4e7d4e17da8c
walter9088 <515771829@qq.com>
2018-05-02 20:06:39 +0800

    first commit

---
+++ webPageTest1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+import urllib2
+from bs4 import BeautifulSoup
+import re
+
+url = "http://www.jxsggzy.cn/web/jyxx/002004/002004004/20171130/926e6b9b-17e6-4dbc-8f91-53076c4422a3.html"
+
+content = urllib2.urlopen(url)
+
+soup = BeautifulSoup(content)
+
+tag = soup.select("div.article-info")
+
+print tag[0].text.strip()
